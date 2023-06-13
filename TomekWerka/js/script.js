7db122fc24de9074571a126d717c1afbc2e77c29
Robert <130874846+RobFrontend@users.noreply.github.com>
2023-06-13 20:50:29 +0200

    Add files via upload

---
+++ TomekWerka/js/script.js
@@ -0,0 +1,81 @@
+//////////////////////////////////////////////////
+//// Aktualny rok
+// const yearEl = document.querySelector(".year");
+// const currentYear = new Date().getFullYear();
+// yearEl.textContent = currentYear;
+/////////////////////////////////////////////////
+// Mobilna nawigacja menu
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Mobilna nav
+const allLinks = document.querySelectorAll(".smooth:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to the top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) document.body.classList.add("sticky");
+    else document.body.classList.remove("sticky");
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-90px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Animated jobs
+
+(() => {
+  const words = [
+    "handlowej",
+    "usługowej",
+    "e-commerce",
+    "dropshipping",
+    "zdrowotnej",
+    "beauty",
+    "finansowej",
+    "ubezpieczeniowej",
+  ];
+  let i = 0;
+  const selected = document.querySelector("#words");
+  setInterval(() => {
+    selected.textContent = words[i];
+    i = (i + 1) % words.length;
+  }, 1500);
+})();
